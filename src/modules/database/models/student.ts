7a3d524dd3a8ff7e88aa5c02ad516c58f292fdd5
Kaique Covo <kaique.kng@gmail.com>
2021-09-02 23:18:56 -0300

    feat: Students model

---
+++ src/modules/database/models/student.ts
@@ -0,0 +1,36 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { Field, ObjectType } from '@nestjs/graphql';
+
+@ObjectType()
+@Entity({ name: 'students' })
+export default class Student {
+  @Field()
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field()
+  @Column()
+  name: string;
+
+  @Field()
+  @Column()
+  cpf: string;
+
+  @Field()
+  @Column()
+  email: string;
+
+  @Field()
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @Field()
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
+}
